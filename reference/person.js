b93e2948cc1596e5704ac561786f6fab57d2db86
yashwanthg7 <yashwanth18183@gmail.com>
2023-04-19 22:25:09 +0530

    Commit 4

--- reference/person.js
+++
@@ -1,5 +0,0 @@
-const person = {
-    name: "Yashwanth Reddy",
-    age :"21"
-}
-module.exports = person;
--- reference/url_demo.js
+++
@@ -1,5 +0,0 @@
-const url = require('url');
-const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active')
-
-//Serialized URL
-console.log(myUrl.href);
